f23da72ca58ce1109a69654a550d1853ce797769
Paul Bachmann <pa.bachmann@gmx.net>
2024-07-09 22:56:21 +0200

    Added corresponder dicts.

--- 06/ASSEMBLER/Assembler.py
+++ 06/ASSEMBLER/Assembler.py
@@ -1,6 +1,57 @@
 from Parser import Parser
 from Code import Code
 
+comp = {
+    '0':    '0101010',
+    '1':    '0111111',
+    '-1':   '0111010',
+    'D':    '0001100',
+    'A':    '0110000',
+    'M':    '1110000',
+    '!D':   '0001101',
+    '!A':   '0110001',
+    '!M':   '1110001',
+    '-D':   '0001111',
+    '-A':   '0110011',
+    '-M':   '1110011',
+    'D+1':  '0011111',
+    'A+1':  '0110111',
+    'M+1':  '1110111',
+    'D-1':  '0001110',
+    'A-1':  '0110010',
+    'M-1':  '1110010',
+    'D+A':  '0000010',
+    'D+M':  '1000010',
+    'D-A':  '0010011',
+    'D-M':  '1010011',
+    'A-D':  '0000111',
+    'M-D':  '1000111',
+    'D&A':  '0000000',
+    'D&M':  '1000000',
+    'D|A':  '0010101',
+    'D|M':  '1010101'
+}
+
+dest = {
+    'M':     '001',
+    'D':     '010',
+    'DM':    '011',
+    'A':     '100',
+    'AM':    '101',
+    'AD':    '110',
+    'ADM':   '111'
+}
+
+jump = {
+    'JGT':   '001',
+    'JEQ':   '010',
+    'JGE':   '011',
+    'JLT':   '100',
+    'JNE':   '101',
+    'JLE':   '110',
+    'JMP':   '111'
+}
+
 class Assembler():
     def __init__(self, asmFile):
         self.Code = Code()
@@ -18,8 +69,14 @@ class Assembler():
                 case "A_INSTRUCTION":
                     self.Parser.symbol()
                 case "C_INSTRUCTION":
-                    self.Parser.dest()
-                    self.Parser.comp()
-                    self.Parser.jump()
+                    try:
+                        dest = self.Code.dest(self.Parser.dest())
+                    except:
+                        dest = '000'
+                    try:
+                        jump = self.Code.jump(self.Parser.jump())
+                    except:
+                        jump = '000'
+                    comp = self.Code.comp(self.Parser.comp())
                 case "L_INSTRUCTION":
                     self.Parser.symbol()
--- 06/ASSEMBLER/Code.py
+++ 06/ASSEMBLER/Code.py
@@ -1,12 +1,50 @@
 class Code():
-    def __init__(self):
-        pass
+    def __init__(self, comp : dict, dest : dict, jump : dict):
+        self.comp = comp
+        self.dest = dest
+        self.jump = jump
 
-    def dest(self, mnemonic : str):
-        pass
+    def dest(self, mnemonic : str) -> str:
+        match(mnemonic):
+            case 'M':
+                return '001'
+            case 'D':
+                return '010'
+            case 'DM':
+                return '011'
+            case 'A':
+                return '100'
+            case 'AM':
+                return '101'
+            case 'AD':
+                return '110'
+            case 'ADM':
+                return '111'
 
-    def comp(self, mnemonic : str):
-        pass
+    def comp(self, mnemonic : str) -> str:
+        match(mnemonic):
+            case '0':
+                return '0101010'
+            case '1':
+                return '0111111'
+            case '-1':
+                return '0111010'
+            case 'D':
+                return ''
 
-    def jump(self, mnemonic : str):
-        pass
+    def jump(self, mnemonic : str) -> str:
+        match(mnemonic):
+            case 'JGT':
+                return '001'
+            case 'JEQ':
+                return '010'
+            case 'JGE':
+                return '011'
+            case 'JLT':
+                return '100'
+            case 'JNE':
+                return '101'
+            case 'JLE':
+                return '110'
+            case 'JMP':
+                return '111'
